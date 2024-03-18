4438eea2e6cb93cf0c7734a9060fb7d7d290b861
Jeremy Nguyen <89870941+jeremu2907@users.noreply.github.com>
2024-03-18 13:19:43 -0500

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.node import Node
@@ -17,39 +18,41 @@ class LinuxRouter(Node):
 
 class NetworkTopo(Topo):
     def build(self, **_opts):
-        # Add 2 routers in two different subnets
-        r1 = self.addHost('r1', cls=LinuxRouter, ip='10.0.0.1/24')
+        r1 = self.addHost('r1', cls=LinuxRouter, ip='10.0.0.1/8')
 
-        # Add 2 switches
         s1 = self.addSwitch('s1')
 
-        # Add host-switch links in the same subnet
-        self.addLink(s1, r1,
-                intfName2='r1-eth0',
-                params2={
-                    'ip': '10.0.0.100/24'
-                }
-        )
-
         # Adding hosts specifying the default route
-        h1 = self.addHost(name='h1')
-        h2 = self.addHost(name='h2', ip='10.0.0.1/24');
-        h3 = self.addHost(name='h3', ip='10.0.0.1/24');
-        #h2 = self.addHost(name='h2')
+        h1 = self.addHost(name='h1',
+                          ip='10.1.0.252/24',
+                          defaultRoute='via 10.1.0.1')
+        h2 = self.addHost(name='h2',
+                          ip='10.0.0.251/24',
+                          defaultRoute='via 10.0.0.1')
+        h3 = self.addHost(name='h3',
+                          ip='10.0.0.253/24',
+                          defaultRoute='via 10.0.0.1')
+
+        # Add host-switch links in the same subnet
+        self.addLink(s1,
+                     r1,
+                     intfName2='r1-eth1',
+                     params2={'ip': '10.0.0.1/24'})
+
+        self.addLink(h1,
+                     r1,
+                     intfName2='r1-eth2',
+                     params2={'ip': '10.1.0.1/24'})
 
         # Add host-switch links
-        self.addLink(h1, r1, params2={'ip': '172.0.0.0/24'}, intfName='r1-eth1')
         self.addLink(h2, s1)
         self.addLink(h3, s1)
-
 
 def run():
     topo = NetworkTopo()
     net = Mininet(topo=topo)
-
-    # Add routing for reaching networks that aren't directly connected
-    info(net['r1'].cmd("route"))
     net.start()
+    info(net.pingAll())
     CLI(net)
     net.stop()
 
